24776185f2af9a9136b6c0db71bd4ad647ad53f1
moro <62976395+morogoha1995@users.noreply.github.com>
2020-06-01 04:18:51 +0900

    Added createFontStyle().

--- src/utils.ts
+++ src/utils.ts
@@ -9,3 +9,19 @@ export const determineZoom = (width: number, height: number): number => {
 
   return 1
 }
+
+export const createFontStyle = (color: string, fontSize = 24, isStroke = true) => {
+  const fontStyle: any = {
+    color: color,
+    fontFamily: "Meiryo",
+    fontSize: `${fontSize}px`,
+    fontStyle: "bold"
+  }
+
+  if (isStroke) {
+    fontStyle.stroke = "white"
+    fontStyle.strokeThickness = 6
+  }
+
+  return fontStyle
+}
