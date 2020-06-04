f7b76d227b54d853b48a110958ae2516ffab08bc
moro <62976395+morogoha1995@users.noreply.github.com>
2020-06-04 10:26:14 +0900

    Changed field.

--- src/objects/field.ts
+++ src/objects/field.ts
@@ -1,22 +1,67 @@
-import { TILE_SIZE, SIDE_BAR_WIDTH } from "../constants"
+import { TILE_SIZE, SIDE_BAR_WIDTH, HALF_TILE_SIZE } from "../constants"
 
 export class Field {
-  private indexes = [
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-    [0, 1, 0, 1, 0],
-    [1, 0, 1, 0, 1],
-  ]
-
-  layer: Phaser.Tilemaps.StaticTilemapLayer
+  private map: number[][]
 
   constructor(scene: Phaser.Scene) {
-    const map = scene.make.tilemap({ data: this.indexes, tileWidth: TILE_SIZE, tileHeight: TILE_SIZE })
-    const tiles = map.addTilesetImage("tiles")
-    this.layer = map.createStaticLayer(0, tiles, SIDE_BAR_WIDTH, 0)
+    const col = 5,
+      row = 7,
+      indexes: number[][] = []
+
+    let x = SIDE_BAR_WIDTH,
+      y = 0,
+      imageFrame = 0
+
+    for (let i = 0; i < row; i++) {
+      indexes[i] = []
+      for (let j = 0; j < col; j++) {
+        indexes[i][j] = 0
+        scene.add.image(x, y, "tiles", imageFrame)
+          .setOrigin(0)
+        imageFrame = imageFrame === 0 ? 1 : 0
+        x += TILE_SIZE
+      }
+
+      x = SIDE_BAR_WIDTH
+      y += TILE_SIZE
+    }
+
+    this.map = indexes
+  }
+
+  getTileCenterPos(x: number, y: number): { [key: string]: number } | false {
+    if (x < SIDE_BAR_WIDTH)
+      return false
+
+    x -= SIDE_BAR_WIDTH
+
+    const index = this.getIndex(x, y)
+
+    console.log(index)
+
+    return {
+      x: TILE_SIZE * index.x + HALF_TILE_SIZE + SIDE_BAR_WIDTH,
+      y: TILE_SIZE * index.y + HALF_TILE_SIZE
+    }
+  }
+
+  canPutFrog(x: number, y: number): boolean {
+    if (x < SIDE_BAR_WIDTH)
+      return false
+
+    x -= SIDE_BAR_WIDTH
+
+    const index = this.getIndex(x, y)
+    return this.map[index.y][index.x] === 0
+  }
+
+  private getIndex(x: number, y: number) {
+    const xIndex = Math.floor(x / TILE_SIZE),
+      yIndex = Math.floor(y / TILE_SIZE)
+
+    return {
+      x: xIndex,
+      y: yIndex
+    }
   }
 }
--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -63,12 +63,12 @@ export class Game extends Phaser.Scene {
     this.selectedFrog?.setPosition(x, y)
 
 
-    const tile = this.field.layer.getTileAtWorldXY(x, y)
+    const pos = this.field.getTileCenterPos(x, y)
 
-    if (!tile)
+    if (!pos)
       return
 
-    this.frogSample?.setPosition(tile.getCenterX(), tile.getCenterY())
+    this.frogSample?.setPosition(pos.x, pos.y)
       .setVisible(true)
   }
 
@@ -76,12 +76,12 @@ export class Game extends Phaser.Scene {
     if (!this.selectedFrog)
       return
 
-    const tile = this.field.layer.getTileAtWorldXY(x, y)
+    const pos = this.field.getTileCenterPos(x, y)
 
-    if (!tile)
+    if (!pos)
       return
 
-    this.frogGroup.add(new Frog(this, tile.getCenterX(), tile.getCenterY(), this.selectedFrog.name))
+    this.frogGroup.add(new Frog(this, pos.x, pos.y, this.selectedFrog.name))
     this.removeFrog()
   }
 }
