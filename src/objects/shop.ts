6662e2c9aa7af39d5cf808aacdb4b51ab8669426
moro <62976395+morogoha1995@users.noreply.github.com>
2020-06-14 00:04:18 +0900

    Added Shop.goldText

--- src/objects/shop.ts
+++ src/objects/shop.ts
@@ -6,29 +6,31 @@ import { createFontStyle } from "../utils"
 export class Shop {
   private gold = 50
   private frogs: Frogs = {}
+  private goldText: Phaser.GameObjects.Text
 
   constructor(scene: Phaser.Scene) {
-    // 武器一覧の表示
     const fds: any = frogDatas,
       height = 100,
       halfHeight = height / 2,
       imgSize = 70,
       padding = 6,
-      fontSize = 36,
+      fontSize = 28,
       textX = imgSize + padding,
-      priceTextY = halfHeight
+      textY = halfHeight
 
     let y = 0
 
+    // 所持金の表示
+    scene.add.text(padding, textY, "所持金:", createFontStyle("black", 20)).setOrigin(0, 0.5)
+    this.goldText = scene.add.text(90, textY, "0G", createFontStyle("orange", fontSize)).setOrigin(0, 0.5)
+    y += height + padding
+
+    // 武器一覧の表示
     for (const key in fds) {
       const fd = fds[key],
-
         img = scene.add.image(padding, halfHeight, key).setDisplaySize(imgSize, imgSize).setOrigin(0, 0.5),
-
-        priceText = scene.add.text(textX, priceTextY, `${fd.price}G`, createFontStyle("orange", fontSize)).setOrigin(0, 0.5),
-
+        priceText = scene.add.text(textX, textY, `${fd.price}G`, createFontStyle("orange", fontSize)).setOrigin(0, 0.5),
         zone = scene.add.zone(0, 0, SIDE_BAR_WIDTH, height).setOrigin(0, 0).setInteractive(),
-
         container = scene.add.container(0, y, [img, priceText, zone])
 
       this.frogs[key] = {
@@ -37,6 +39,11 @@ export class Shop {
       }
       y += height + padding
     }
+
+  }
+
+  setGoldText(value: number) {
+    this.goldText.setText(`${value}G`)
   }
 
   getFrogs(): Frogs {
