64be38362636d226e9c3cf34691b388ec5c3a195
moro <62976395+morogoha1995@users.noreply.github.com>
2020-07-04 04:19:13 +0900

    Changed main.ts zoom to scale.

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import Phaser from "phaser"
 import { WIDTH, HEIGHT } from "./constants"
-import { determineZoom } from "./utils"
 import { Boot } from "./scenes/boot"
 import { Game } from "./scenes/game"
 import { Menu } from "./scenes/menu"
 import { End } from "./scenes/end"
 
 window.onload = () => {
-  const zoom = determineZoom(window.innerWidth, window.innerHeight)
-
   new Phaser.Game({
     type: Phaser.AUTO,
     width: WIDTH,
     height: HEIGHT,
     parent: 'app',
-    zoom: zoom,
+    scale: {
+      mode: Phaser.Scale.FIT,
+      max: {
+        width: WIDTH,
+        height: HEIGHT
+      }
+    },
     physics: {
       default: "arcade",
       arcade: {
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,5 @@
 import { WIDTH, HEIGHT, FONT_SIZE } from "./constants"
 
-export const determineZoom = (width: number, height: number): number => {
-  const zooms = [1, 0.75]
-
-  for (let zoom of zooms)
-    if (width > WIDTH * zoom && height > HEIGHT * zoom)
-      return zoom
-
-  return 0.5
-}
-
 export const createFontStyle = (color: string, fontSizeRatio = 1, isStroke = true) => {
   const fontStyle: any = {
     color: color,
